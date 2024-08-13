b2f0a8c0d1c3f06406f549f23de2b05cff8f7ca4
quietgong <quietgong@naver.com>
2024-08-13 16:13:04 +0900

    init

---
+++ PYN/control.js
@@ -0,0 +1,46 @@
+// let clue = 0;
+
+// function hint01 () {
+//     var info = document.getElementById('info');  
+//     var info_text = document.getElementById('info_context');  
+//     info.style.display ="block";
+//     info_text.innerHTML = "<b>단서0. 아티젝타를 포착하세요!</b></br>그들을 포착하기 위한 전파를 생성하는 와이파이 장치."
+//     clue = 1;
+//     console.log("01");
+// }
+
+// function hint02 () {
+//     var info = document.getElementById('info');  
+//     var info_text = document.getElementById('info_context');  
+//     info.style.display ="block";
+//     info_text.innerHTML = "<b>단서1. 현혹의 순간</b></br>수집된 자료, 파운드 푸티지, 기록<p><p>그들은 보이지 않을 뿐, 무선 인터넷이 가능한 모든 곳에 존재한다. 잠깐의 오류로 그것을 포착했던 순간을 잊을 수 없다. 아주 짧은 순간이었지만, 손에 쥐고 있던 핸드폰 덕분에 이를 담을 수 있었다. 그리고 이 순간을 나만 겪은 것이 아니었다는 것을, 이것이 잠깐의 오류가 아닌 현실보다도 더 커져가는 현실이었음을, 오랫동안의 자료 수집으로 알게 되었다. 나의 기록을 포함해서, 많은 정보들이 그들의 존재를 증명하고 있다. 그들은 그저 존재함을 넘어, 우리를 조종하려 한다."    
+//     clue = 2;
+//     console.log("02");    
+// }
+
+// function hint03 () {
+//     var info = document.getElementById('info');  
+//     var info_text = document.getElementById('info_context');  
+//     info.style.display ="block";
+//     info_text.innerHTML = "<b>단서2. 그것은 오래전부터 시작되었다.</b></br>축적된 데이터로 만들어진 인체 계측도 8장, AI 기술로 복원된 60-90년대 제품 광고 이미지 6장.<p><p>그들은 오래전부터 인간의 신체를 낱낱이 분석해 통계를 냈고, 그 정보는 우리의 소비를 이끌어 내는 전략에 쓰였다. 나는 그 증거 자료들을 오랫동안 수집했다. 그리고 결론에 이르렀다. 우리의 소비는 그들이 시공간을 넘고 온-오프라인을 넘어 번영해 나가는 것을 가능케 한다."
+//     clue = 3;
+//     console.log("03");
+// }
+
+// function hint04 () {
+//     var info = document.getElementById('info');  
+//     var info_text = document.getElementById('info_context');  
+//     info.style.display ="block";
+//     info_text.innerHTML = "<b>단서3. 포착 전파 생선 설계안</b></br>그들을 포착하기 위한 전파를 생성하는 와이파이 장치 설계도.<p><p>그들의 신호는 공공 와이파이가 활발히 사용되는 지점에서 주로 발견된다. 그러나 사람들의 시선이 밀집되어 있는 만큼, 그들을 포착하기란 쉽지 않다. 나는 전시를 빌어, 고립된 틈과 같은 이곳을 방문한 모두가 그들을 잠깐이라도 포착할 수 있는 기회를 마련하려 한다. 지금껏 수집한 정보를 분석하고 취합한 결과, 와이파이 공유기를 개조하여 이 장치를 만들 수 있었다. 세개의 안테나는 와이파이 신호를 중첩시키고 전파를 교란하여 특정한 주파수를 만들어낸다. 아직 불안정하지만, 그들을 포착하는데 성공했다!"    
+//     clue = 4;
+//     console.log("04");
+// }
+
+// function hint05 () {
+//     var info = document.getElementById('info');  
+//     var info_text = document.getElementById('info_context');  
+//     info.style.display ="block";
+//     info_text.innerHTML = "<b>단서 4. 오래된 기록들의 파편</b><p><p>1990.10.05</br>이 몸이 이질적으로 느껴진다.</br>키보드를 두드리는 이 손을 다른 곳에서 지켜보고 있는 듯한 느낌.</br>손가락 끝에 닿는 뜨겁고 매끈한 표면이, 짜여진 코드의 시뮬레이션이라는 감각.</br>어느 순간 모든 표면이 미끄러지고, 모든 마찰이 흩어져 버릴 것 같다.</br>나는 이미 몸을 잃어버린 누군가의 의식일까?</br>내 몸과 내 몸의 모든 감각과 행동은 무언가의 의식이 반영된 결과인가?</br><i>그들은 특정한 움직임을 유도해.</i><p>2009.05.01</br>그들의 형태는 내 몸을 아주 익숙하게 움직이게 한다.</br>이 모든 것을 내 의지와 판단으로 선택했다고 생각했던 적이 있었다.</br><i>그들은 네 생각과 행동을 지배해.</i></br>온전한 내 몸, 건강한 정신, 합리적 선택, 확신에 찬 움직임.</br><i>환상</i><p><p>2017.06.02</br>의심은 찰나였다. 그러나 그 이후, 더 이상 이전과 같은 세계는 없었다.의자는 내 엉덩이와 등을 그의 표현에 닿게 했으며, 컵은 내가 물을 그 오목한 형태에 담게 하고, 내 손을 그 적당한 크기의 고리에 넣어 자기 몸을 들어 올리게 하여, 자신에게 담긴 그 물을 내 입으로 가져가게 했다.</br>"
+//     clue = 5;
+//     console.log("05");
+// }
